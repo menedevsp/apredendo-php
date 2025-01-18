6bac70ead1c35dd51bceaa54c59287ec188be021
Guilherme Menezes <gmenedevs@gmail.com>
2025-01-18 14:51:38 -0300

    conclusão index.php

--- index.php
+++ index.php
@@ -12,8 +12,48 @@
     //Classes
 
 
-    $nome = "Guilherme";
+    $nome = 'Guilherme';
+    $idade = '24'
+    $$Guilherme = 'Roberto Antonio';
 
     echo $nome;
+    echo 'Fala meu amigo '.$nome;
+    echo $$nome;
+    if($idade !== 24){
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+    for($i = 0; $i < 10; $i++){
+        echo $i;
+    }
+
+    $i = 0;
+    while($i < 10){
+        echo $i;
+        $i++;
+    }
+
+    function soma($a, $b){
+        echo $a + $b;
+    }
+    soma(2, 3);
+
+    class Pessoa {
+        public $nome;
+        public $idade;
+
+        public function __construct($nome, $idade){
+            $this->nome = $nome;
+            $this->idade = $idade;
+        }
+
+        public function apresentar(){
+            echo 'Olá, meu nome é '.$this->nome.' e tenho '.$this->idade.' anos';
+        }
+    }
+    $pessoa = new Pessoa('Guilherme', 24);
+    $pessoa->apresentar();
 
 ?>
