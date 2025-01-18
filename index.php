f143cf2e4537817df7f92e0a12a6a82a45f44c96
Guilherme Menezes <gmenedevs@gmail.com>
2025-01-18 14:41:52 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    //PHP - Personal Home Page
+
+    //Variáveis
+    //Variável de variável
+    //Concatenar
+    //Comparar valores
+    //>= <= == !=
+    //=== !==
+    //Looping
+    //Funções
+    //Classes
+
+
+    $nome = "Guilherme";
+
+    echo $nome;
+
+?>
